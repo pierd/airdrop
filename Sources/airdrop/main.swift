b52296285ca62dab628b8fc1b83567cd804fe0da
Jakub Jaroszewski <jakub.jaroszewski@gmail.com>
2019-05-09 21:41:58 +1000

    Add support for local files

--- Sources/airdrop/main.swift
+++ Sources/airdrop/main.swift
@@ -4,13 +4,22 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSSharingServiceDelegate {
     var urls: [NSURL]
 
     init<T: Collection>(urls: T) where T.Element == String {
-        self.urls = urls.map { NSURL.init(string: $0)! }
+        self.urls = urls.map {
+            if $0.starts(with: "file://") || $0.starts(with: "https://") || $0.starts(with: "http://") {
+                return NSURL.init(string: $0)!
+            } else {
+                return NSURL.fileURL(withPath: $0) as NSURL
+            }
+        }
     }
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        let shareService = NSSharingService.init(named: NSSharingService.Name.sendViaAirDrop)
-        shareService!.delegate = self
-        shareService!.perform(withItems: self.urls)
+        if let service = NSSharingService.init(named: NSSharingService.Name.sendViaAirDrop) {
+            service.delegate = self
+            service.perform(withItems: self.urls)
+        } else {
+            exit(-1)
+        }
     }
 
     func sharingService(_ sharingService: NSSharingService, didShareItems items: [Any]) {
